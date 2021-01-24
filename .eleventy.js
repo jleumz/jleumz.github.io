b609915a4b9f9c56ad6987ad1b022e1f9d245ad0
Johannes Leussler <johannes.leussler@gmail.com>
2021-01-24 18:42:37 +0100

    Changed output directory

---
+++ .eleventy.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const moment = require('moment');
+
+
+module.exports = (config) => {
+
+
+    dir: {
+        output: "docs"
+    }
+
+    // Add related posts
+    config.addLiquidShortcode('related', (title, url) => `
+        <a href="${url}" class="related">${title}</a>
+    `);
+
+    // add liquid shortcode for reusable html 
+    config.addLiquidShortcode('test', (param1, param2) => `
+        <p>${param1}: ${param2}</p>
+    `);
+
+    // filters
+    config.addFilter('toUTCString', (date) => {
+        if (date) {
+            const utc = date.toUTCString();
+            return moment.utc(utc).format('MMMM Do YYYY');
+        } else {
+            return '';
+        }
+    });
+
+    // filter for favorite posts
+    config.addFilter('favorites', arr => {
+        // in frontmatter searching for attribute favorite=true
+        const favs = p => p.data.favorite;
+        return arr.filter(favs);
+    });
+
+    // filter for displaying #amount# of arr
+    config.addFilter('take', (arr, amount = 2) => {
+        return arr.slice(0, amount);
+    });
+
+
+    // browsersync config for 404 page
+    config.setBrowserSyncConfig({
+        callbacks: {
+            ready: function (err, bs) {
+
+                bs.addMiddleware("*", (req, res) => {
+                    const content_404 = fs.readFileSync('_site/404.html');
+                    // Add 404 http status code in request header.
+                    res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.end();
+                });
+            }
+        }
+    });
+
+    // add png to get image directory with png in _site
+    // config.setTemplateFormats([ 'png' ])
+};
